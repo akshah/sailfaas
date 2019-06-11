fa6e3f5c0e7c54a3b202cf7ecfaf4b4803f6d4d1
Anant Shah <anant.shah@verizondigitalmedia.com>
2019-06-11 12:14:48 -0700

    Up

--- sailfaas/handler.py
+++ sailfaas/handler.py
@@ -1,15 +1,17 @@
-from flask import make_response
-def handle(request):
+def make_response(body, status):
+    return {"statusCode": status, "body": body.decode("utf-8") }
+
+def handle(request, context):
     """handle a request to the function
     Args:
         request (str): request body
     """
-    val1 = float(request.args.get('val1'))
-    val2 = float(request.args.get('val2'))
-    val3 = float(request.args.get('val3'))
-    val4 = float(request.args.get('val4'))
-    val5 = float(request.args.get('val5'))
-    val6 = float(request.args.get('val6'))
+    val1 = float(request.query.get('val1'))
+    val2 = float(request.query.get('val2'))
+    val3 = float(request.query.get('val3'))
+    val4 = float(request.query.get('val4'))
+    val5 = float(request.query.get('val5'))
+    val6 = float(request.query.get('val6'))
     
     res1 = str(val1*val2/val3)
     res2 = str(val4*val5/val6) 
